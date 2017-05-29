cadbef331b4936eb1b29e62f15a789718bdac4be
Erronsing <garthbjerk@gmail.com>
2017-05-29 16:32:26 +0200

    Added view to create ingredient inside the site.

--- myfridge/urls.py
+++ myfridge/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     url(r'^ingredients$', views.ingredient_list, name='ingredient_list'),
+    url(r'^ingredient/new/$', views.ingredient_new, name='ingredient_new')
 ]
--- myfridge/views.py
+++ myfridge/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.template import loader
 from .models import Ingredient, Recipe, RecipeIngredient
+from .forms import IngredientForm
+
 
 def ingredient_list(request):
 	ingredients = Ingredient.objects.order_by('name')
 	template = loader.get_template('myfridge/ingredient_list.html')
 	context = {'ingredients': ingredients}
 	return render(request, 'myfridge/ingredient_list.html', context)
+
+def ingredient_new(request):
+	if request.method == "POST":
+		form = IngredientForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('ingredient_list')
+	else:
+		form = IngredientForm
+	return render(request, 'myfridge/ingredient_edit.html', {'form': form})
