71f143eeb0ec1d5676d90bde5f63851ba0384e3f
Erronsing <garthbjerk@gmail.com>
2017-05-29 15:46:50 +0200

    Template and url for ingredient list.

--- myfridge/views.py
+++ myfridge/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from django.template import loader
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Create your views here.
+def ingredient_list(request):
+	ingredients = Ingredient.objects.order_by('name')
+	template = loader.get_template('myfridge/ingredient_list.html')
+	context = {'ingredients': ingredients}
+	return render(request, 'myfridge/ingredient_list.html', context)
